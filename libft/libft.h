5f7597c176ddaa1b7619857ce209c6ecf0371313
Taylor Yang <tyang@e1z2r5p35.42.us.org>
2017-12-09 13:05:19 -0800

    fix

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tyang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 22:17:23 by tyang             #+#    #+#             */
-/*   Updated: 2017/12/09 11:17:11 by tyang            ###   ########.fr       */
+/*   Updated: 2017/12/09 12:05:38 by tyang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void				*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t len);
 void				*ft_memchr(const void *src, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
-size_t					ft_strlen(const char *str);
+size_t				ft_strlen(const char *str);
 char				*ft_strdup(const char *s1);
 char				*ft_strcpy(char *dest, const char *src);
 char				*ft_strncpy(char *dest, const char *src, size_t len);
